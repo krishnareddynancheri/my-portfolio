4e86c55383cc81d286f1ccccf5935f2dea57f952
krishna <krishnareddy.nancheri.com>
2022-04-30 23:11:40 +0530

    profile adjustments

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -10,11 +10,11 @@ export const Profile = () => {
           src={require("../../images/dp.jpg")} />
       </section>
       <section>
-        <section style={{paddingBottom:"2%"}}>
-          <section><b style={{fontSize:"1.5rem"}}>Krishnareddy</b></section>
+        <section style={{ paddingBottom: "2%" }}>
+          <section><b style={{ fontSize: "1.5rem" }}>Krishnareddy</b></section>
           <small>software developer</small>
         </section>
-        <section>A Developer with strong passion for self-improvement. I have strong problem solving skills & bring energy, patience, positivity and cultural values to the team.</section>
+        <section style={{ paddingBottom: "20px" }}>A Developer with strong passion for self-improvement. I have strong problem solving skills & bring energy, patience, positivity and cultural values to the team.</section>
       </section>
     </div>
   )
