b94b6bd43c3ea9843105ddd14abe590d2c8ddd89
krishna <krishnareddy.nancheri.com>
2022-05-06 00:00:17 +0530

    data alignment changes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -6,14 +6,16 @@ export const Contact = () => {
   return (
     <div className='contact-container' style={{ display: "grid", gridTemplateColumns: "1fr" }}>
       <div style={{ fontWeight: "bold" }}>Contact Info</div>
-      {data.contact.map(i => {
-        return (
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 2fr", padding: "1% 5% 1% 0%" }}>
-            <label style={{ fontWeight: "bold" }}>{i.label}</label>
-            {i.type === "link" ? <a rel="noreferrer" target={"_blank"} href={i.value} style={{ fontWeight: "bold" }}>{i.value}</a> : <label style={{ fontWeight: "bold" }}>{i.value}</label>}
-          </div>
-        )
-      })}
+      <div style={{ fontSize: "0.9rem", opacity: "0.8" }}>
+        {data.contact.map(i => {
+          return (
+            <div style={{ display: "grid", gridTemplateColumns: "1fr 2fr", padding: "1% 5% 1% 0%" }}>
+              <label style={{ fontWeight: "bold" }}>{i.label}</label>
+              {i.type === "link" ? <a rel="noreferrer" target={"_blank"} href={i.value} style={{ fontWeight: "bold" }}>{i.value}</a> : <label style={{ fontWeight: "bold" }}>{i.value}</label>}
+            </div>
+          )
+        })}
+      </div>
       <a rel="noreferrer" style={{ marginTop: "10px" }} href='https://drive.google.com/file/d/1EbZqn9PD8amjXCCiDbtTU1mEhx7lTGI_/view?usp=sharing' target={"_blank"}>download cv</a>
     </div>
   )
--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -44,7 +44,7 @@ const Employment = () => {
 }
 const TechStack = ({ name, rating }) => {
   return (
-    <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center", maxWidth: "300px" }}>
+    <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center", maxWidth: "300px",padding:"3px 0px",fontSize:"0.9rem",opacity:"0.8" }}>
       <div><label>{name}</label></div>
       <div style={{ display: 'flex' }}>
         {[0, 1, 2, 3, 4, 5, 6].map(i => <RatingDot fillColor={i <= rating ? "#757575" : "#d6d6d6"} />)}
@@ -67,7 +67,7 @@ const TwoColSection = ({ header, subHeader, rightHeader }) => {
 }
 const RatingDot = ({ fillColor }) => {
   return (
-    <div style={{ height: "15px", width: "15px", backgroundColor: fillColor, margin: "5px", borderRadius: "100%" }}></div>
+    <div style={{ height: "13px", width: "13px", backgroundColor: fillColor, margin: "4px", borderRadius: "100%" }}></div>
   );
 }
 const Education = () => {
