b662cf07a89f29f5e7fb01b9b411bbe264e5aae5
krishna <krishnareddy.nancheri.com>
2022-05-06 00:01:22 +0530

    Pascal case

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -12,7 +12,7 @@ export const Profile = () => {
       <section>
         <section style={{ paddingBottom: "2%" }}>
           <section><b style={{ fontSize: "1.5rem" }}>Krishnareddy</b></section>
-          <small>software developer</small>
+          <small>Software Developer</small>
         </section>
         <section style={{ paddingBottom: "20px" }}>A Developer with strong passion for self-improvement. I have strong problem solving skills & bring energy, patience, positivity and cultural values to the team.</section>
       </section>
